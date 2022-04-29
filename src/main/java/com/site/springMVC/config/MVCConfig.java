e029b90cb596e25bede00ebc5133cda8fcba3431
Miqelad <88131800+Miqelad@users.noreply.github.com>
2022-04-29 12:08:36 +0300

    Update MVCConfig.java

--- src/main/java/com/site/springMVC/config/MVCConfig.java
+++ src/main/java/com/site/springMVC/config/MVCConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class MVCConfig implements WebMvcConfigurer {
     @Value("${upload.path}")
     private String uploadPath;
